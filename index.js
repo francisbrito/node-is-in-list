10414f1b67b491e5773833d0b097492d93c9ae1c
Francis Brito <francis@instacarro.com>
2016-04-15 10:40:21 -0400

    Add support for searching a value item.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 'use strict';
+function isInList(list, item) {
+  const matchingItem = list.filter(function (li) {
+    return li === item;
+  })[0];
+
+  return !!matchingItem;
+}
+
+module.exports = isInList;
--- tests.js
+++ tests.js
@@ -1,1 +1,13 @@
 'use strict';
+var test = require('tape');
+
+var isInList = require('./');
+
+test('it can find an item.', function (t) {
+  const item = 'bar';
+  const list = ['foo', 'bar', 'baz'];
+
+  t.ok(isInList(list, item), 'should find item.');
+
+  t.end();
+});
