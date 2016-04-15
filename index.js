1a3d4e9baf6574f4cd927476f6fb60cd4b98912d
Francis Brito <francis@instacarro.com>
2016-04-15 11:20:31 -0400

    Add support for currying.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 'use strict';
 function isInList(list, item) {
-  var matchingItem = list.filter(function (li) {
+  var arity = arguments.length;
+  var innerArgs;
+  var wrapperArgs;
+  var matchingItem;
+
+  wrapperArgs = Array.prototype.slice.call(arguments);
+
+  if (arity < 2) {
+    return function () {
+      innerArgs = Array.prototype.slice.call(arguments);
+
+      return isInList.apply(null, [].concat(wrapperArgs).concat(innerArgs));
+    };
+  }
+
+  matchingItem = list.filter(function (li) {
     return li === item;
   })[0];
 
--- tests.js
+++ tests.js
@@ -11,3 +11,14 @@ test('it can find an item.', function (t) {
 
   t.end();
 });
+
+test('it is curried.', function (t) {
+  var primes = [2, 3, 5, 7, 11];
+  var isInPrimeList = isInList(primes);
+
+  t.ok(isInPrimeList.call, 'should return a function.');
+  t.ok(isInPrimeList(2), 'should return `true` if item is in list.');
+  t.notOk(isInPrimeList(1), 'should return `false` if item is not in list.');
+
+  t.end();
+});
